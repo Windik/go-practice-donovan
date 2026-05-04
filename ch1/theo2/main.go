c99094fcd54d9c0d2be782ff8d72ef6db0da64f2
Sergey Bogachev <wlionheart@gmail.com>
2026-05-04 15:51:34 +0300

    feat(ch1): add ch1/echo2 example with comments

---
+++ ch1/theo2/main.go
@@ -0,0 +1,22 @@
+package main
+
+// Go program to concatenate command-line arguments
+// uses a for loop with range to iterate over command-line arguments
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// Initialize an empty string and a separator (space) for the concatenated string.
+	s, sep := "", " "
+
+	// Iterate over all command-line arguments using a for loop with range.
+	for _, arg := range os.Args[1:] {
+        s += sep + arg
+		sep = " "
+    }
+
+	// Print the concatenated string.
+	fmt.Print(s)
+}
